52bbb26a614b921774367bdbff78abd345b137db
Tyler Emmerson <TylerEmmerson0216@gmaile.com>
2022-06-24 15:56:56 -0700

    reduce webpage to one pizza, with room to have set completed pizza's added.

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,48 @@
 // BUSINESS LOGIC
-function Pizza(toppings, size, price, totalPrice) {
-  this.toppings = [];
-  this.size = 0;
-  this.sauce = 0;
-  this.price = 0;
-  this.cartTotal = 0;
-}
-const size = [
-  {size: "8", price: 4},
-  {size: "12", price: 6},
-  {size: "20", price: 10}
-]
-const sauce = [
-  {sauce: "marinara", price: 1},
-  {sauce: "pesto", price: 1},
-  {sauce: "alfredo", price: 1}
-]
-
-Pizza.prototype.cartTotal = function() {
-  let cartTotal = 0;
-  for (let i = 0; i < size.length; i++) {
-    if (this.size === size[i].size){
-      cartTotal += size[i].price;
-    }
-  }
-  for (let i = 0; i < sauce.length; i++) {
-    if (this.sauce === sauce[i].sauce){
-      cartTotal += sauce[i].price;
-    }
-  }
-  for (let i = 0; i < this.toppings.length; i++) {
-    cartTotal += this.toppings[i].length;
-  }
-  console.log(cartTotal);
-  return cartTotal;
+function Pizza(toppings, size, sauce, price) {
+  this.toppings = toppings;
+  this.size = size;
+  this.sauce = sauce;
+  this.price = price;
+  this.cartTotal = price;
 }
 // UI LOGIC
-$(document).ready(function() {
+  $(document).ready(function() {
+    $("form#pizza-order-form").submit(function(event) {
+      event.preventDefault();
+      const selectedSize = document.getElementById("sizeList");
+      const selectedSauce = document.getElementById("sauceList");
+      const selectedToppings = [];
+      $('input[name="toppingList"]:checked').each(function(){
+        selectedToppings.push($(this).val());
+      });
+      //set price based on size
+      if (selectedSize === "small") {
+        sizePrice = 4;
+      }
+      else if (selectedSize === "medium") {
+        sizePrice = 6;
+      }
+      else if (selectedSize === "large") {
+        sizePrice = 8;
+      }
+      //set price based on sauce selected
+      if (selectedSauce === "marinara") {
+        saucePrice = 2;
+      }
+      else (sizePrice = 3);
+      //set price based on number of toppings selected
+      toppingsPrice = 10;
+      //calculate total price
+      totalPrice = sizePrice + saucePrice + toppingsPrice;
+      //create pizza with selected options
+      let newPizza = Pizza(selectedToppings, selectedSize, selectedSauce, totalPrice)
 
-}
-// add to cart button
-$("#cart-button").click(function(event) {
-  event.preventDefault();
-  // will have the cart-button append the added toppings/sizes/sauce to the cart
+      //create text label to display in-cart
+      addedPizza += newPizza.size + ", " + newPizza.sauce + ", " + newPizza.totalPrice + "<br>"+ toString(newPizza.toppings) + "<br><br>"
+
+
+      $("#newPizza").html(addedPizza);
+  });
+});
+// will have the cart-button append the added toppings/sizes/sauce to the cart
